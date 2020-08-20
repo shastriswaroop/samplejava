96f023c96d11d2308dfaf2a40f79c28f5264ab98
shastriswaroop <shastriswaroop@gmail.com>
2020-08-20 13:49:20 +0530

    example of 2 level groupby

--- java8features/src/main/java/shastri/swaroop/streams_terminal/StreamGroupingByType2Example.java
+++ java8features/src/main/java/shastri/swaroop/streams_terminal/StreamGroupingByType2Example.java
@@ -1,4 +1,37 @@
 package shastri.swaroop.streams_terminal;
 
+import shastri.swaroop.data.Student;
+import shastri.swaroop.data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+
+
 public class StreamGroupingByType2Example {
+
+    public static void printGroupByLevel1(){
+        Map<Integer, List<Student>> stdMap =
+        StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getGradeLevel));
+
+        System.out.println(stdMap);
+
+    }
+    public static void printGroupByLevel2(){
+        Map<Integer, Map<String, List<Student>>> stdMap =
+                StudentDataBase.getAllStudents()
+                        .stream()
+                        .collect(groupingBy(Student::getGradeLevel,groupingBy(student -> student.getGpa()>3.5?"Outstanding":"Average")));
+
+        System.out.println(stdMap);
+
+    }
+
+
+    public static void main(String[] args) {
+        printGroupByLevel2();
+    }
 }
